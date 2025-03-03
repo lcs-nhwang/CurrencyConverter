c48b2fc01cc36c45e9775ab24ec0bc49037d97be
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-03-03 14:38:43 -0500

    imported code for model and viewmodel from playground

--- CurrencyConverter/Models/Currency.swift
+++ CurrencyConverter/Models/Currency.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Currency: Identifiable {
+    //MARK: Stored Properties
+    let id = UUID()
+    var cad: Double
+    //MARK: Computed Properties
+    var usd: Double {
+        return cad*1.4438
+    }
+    var ukPound: Double {
+        return cad*1.8177
+    }
+    var euro: Double {
+        return cad*1.5007
+    }
+}
--- CurrencyConverter/ViewModels/CurrencyViewModel.swift
+++ CurrencyConverter/ViewModels/CurrencyViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+@Observable
+class CurrencyViewModel {
+    //MARK: Stored Properties
+    var resultHistory: [Currency] = []
+    var providedCad: String
+    var recoverySuggestion: String = ""
+    //MARK: Computed Properties
+    var currency: Currency? {
+        guard let cad = Double(providedCad), cad > 0 else {
+            recoverySuggestion = "Please provide a correct value of currency."
+            return nil
+        }
+        recoverySuggestion = ""
+        return Currency(cad: cad)
+    }
+    //MARK: Initializers
+    init(
+        providedCad: String = "",
+        recoverySuggestion: String = ""
+    ) {
+        self.providedCad = providedCad
+        self.recoverySuggestion = recoverySuggestion
+    }
+    func saveResult() {
+        if let currency = self.currency {
+            self.resultHistory.insert(currency, at: 0)
+        }
+    }
+}
