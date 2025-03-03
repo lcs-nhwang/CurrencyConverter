ee0fd95720c38ecec2ac61d4cf45bb90c420398b
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-03-03 14:34:40 -0500

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Nicholas Hwang on 3/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
